32c1ff0940ee203901e5980ebf15c164d75acb55
서지훈 <suh1027@naver.com>
2020-04-17 15:31:36 +0900

    3D FPS Survival Game ~#17
    Item Rock 구현 
    Item script -> ScriptableObject 개념 이해 ++
    ActionController를 통해 아이템 반응 추가
    UI Text로 출력
    기존 Rock을 Destroy시에 일정수량 Item이 Drop되도록 설정
    <Color = Yellow> 가 먹히지 않음 색상 변경 필요 + 세세한 폰트 조정

---
+++ 3D FPS Survival Game/Assets/Scripts/ActionController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionController : MonoBehaviour
+{
+    [SerializeField]
+    private float range; // 습득 가능한 최대 거리.
+
+    private bool pickUpActivated = false; // 습득 가능할 시 True;
+    
+    // Ray
+    private RaycastHit hitInfo; // 충돌체 정보 저장
+    
+    [SerializeField]
+    private LayerMask layerMask; // Layermask???? -> 아이템 레이어에만 반응하도록 레이어 마스크를 설정 ??
+
+    [SerializeField]
+    private Text actionText;
+    
+
+    void Update()
+    {
+        TryAction();
+        CheckItem();
+    }
+
+    private void TryAction()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            CheckItem();
+            CanPickUp();
+        }
+    }
+
+    private void CheckItem()
+    {
+        //transform.forward == transform.TransformDirection(Vector3.forward)
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hitInfo, range, layerMask))
+        {
+            if (hitInfo.transform.tag == "Item")
+            {
+                ItemInfoAppear();
+            }
+        }
+        else
+            InfoDisappear();
+    }
+
+    private void ItemInfoAppear()
+    {
+        pickUpActivated = true;
+        actionText.gameObject.SetActive(true);
+        actionText.text = hitInfo.transform.GetComponent<ItemPickUp>().item.itemName 
+            + " 획득" + "<color = yellow>" + "(E)" + "</color>";
+    }
+
+    private void InfoDisappear()
+    {
+        pickUpActivated = false;
+        actionText.gameObject.SetActive(false);
+    }
+
+    private void CanPickUp()
+    {
+        if (pickUpActivated)
+        {
+            if(hitInfo.transform != null)
+            {
+                Debug.Log(hitInfo.transform.GetComponent<ItemPickUp>().item.itemName
+            + " 획득 했습니다.");
+                Destroy(hitInfo.transform.gameObject);
+                InfoDisappear();
+
+            }
+        }
+    }
+}
---
+++ 3D FPS Survival Game/Assets/Scripts/Item.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "New Item/item")]
+public class Item : ScriptableObject 
+    // Scriptableobject -> 게임 오브젝트에 붙여서 사용할 필요가 없는 오브젝트를 만들기 위해 씀
+    // monobehavi.. 게임 오브젝트에 붙여 사용
+{
+    public string itemName;
+    public ItemType itemType; // 아이템의 유형 itemtype.Equipment ....
+    public Sprite itemImage; // 아이템 이미지 (인벤토리에서)
+    public GameObject itemPrefab; // 아이템의 프리팹
+
+    public string weaponType; // 무기 유형.
+
+    public enum ItemType // 아이템 타입 열거형 변수
+    {
+        Equipment,
+        Used,
+        Ingredient,
+        ETC
+    }
+
+}
