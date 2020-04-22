0a00886abcb39f588fc50ce7f2b54511bf6642b4
서지훈 <suh1027@naver.com>
2020-04-22 18:11:50 +0900

    3D FPS Survival Game #37
    START Scene 구현 (scenemanager)

---
+++ 3D FPS Survival Game/Assets/Scripts/UI Script/Title.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    public string sceneName = "GameStage";//다음 씬으로 넘어갈 함수
+
+    public void ClickStart()
+    {
+        Debug.Log("로딩");
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ClickLoad()
+    {
+        Debug.Log("로드");
+    }
+
+    public void ClickExit()
+    {
+        Debug.Log("게임 종료");
+        Application.Quit();
+    }
+}
